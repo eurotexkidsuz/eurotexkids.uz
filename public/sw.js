0d012fe126dbde9c73b88f7c1a0ccf913a237cc5
Abdulaziz701UZB <Abdulaziz701UZB>
2026-09-18 15:48:46 +0500

    style: overhaul storesModal and hero carousel image fit, eliminate inline styles

--- public/sw.js
+++ public/sw.js
@@ -24,9 +24,9 @@ const OFFLINE_HTML = `<!DOCTYPE html>
 </body>
 </html>`;
 
-const CACHE_NAME = "eurotex-v6-precache";
+const CACHE_NAME = "eurotex-v7-precache";
 const CRITICAL_ASSETS = [
-  "/style.css?v=331.0.0",
+  "/style.css?v=332.0.0",
   "/images/eurotex-logo.png",
   "/manifest.json",
 ];
